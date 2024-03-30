e829e7ce039ebe14ab0693b2712648819e946c35
AONE8 <malyshko.dp@gmail.com>
2024-03-30 15:52:18 +0200

    first commit

---
+++ backend/src/main/java/ua/andriimalyshko/backend/courses/controller/CourseController.java
@@ -0,0 +1,17 @@
+package ua.andriimalyshko.backend.courses.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.andriimalyshko.backend.courses.bean.Course;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @GetMapping("/courses")
+    public List<Course> getAllCorses() {
+        return Arrays.asList(new Course(1, "First course", "Some description"));
+    }
+
+}
